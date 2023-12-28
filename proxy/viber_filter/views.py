3003b66c9397ba73c419744e6a8caa67a30cc8cd
Mikhail Noskov <noskovmm@gmail.com>
2023-12-28 22:38:25 +0100

    feat.(viber_filter:views): ViberMessage view created

--- proxy/viber_filter/views.py
+++ proxy/viber_filter/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 
-# Create your views here.
+
+class ViberMessage(viewsets.ViewSet):
+    def create(self, request):
+        data = request.data
+        print(data)
+        return Response(status=status.HTTP_200_OK)
